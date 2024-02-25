1f9c45d1a24048558d800ff5b0d875131f047298
hotequil <hotequil@protonmail.com>
2024-02-24 22:46:27 -0300

    feat: added ModelFactory

---
+++ factories/ModelFactory.py
@@ -0,0 +1,17 @@
+from models.ai_models import BeanLeafModel, PotatoLeafModel, TomatoLeafModel
+
+class ModelFactory:
+    model_registry = {
+        "bean_leaf": BeanLeafModel,
+        "potato_leaf": PotatoLeafModel,
+        "tomato_leaf": TomatoLeafModel
+    }
+
+    @staticmethod
+    def get_model(model_type):
+        try:
+            model_class = ModelFactory.model_registry[model_type]
+
+            return model_class()
+        except KeyError:
+            raise ValueError(f"Model type \"{model_type}\" not supported")
