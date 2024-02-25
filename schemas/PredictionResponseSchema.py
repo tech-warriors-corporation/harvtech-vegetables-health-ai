ae0faaae92a3913742036eb522fb996468c69f51
hotequil <hotequil@protonmail.com>
2024-02-25 08:38:31 -0300

    feat: added request and schemas

---
+++ schemas/PredictionResponseSchema.py
@@ -0,0 +1,18 @@
+from marshmallow import Schema, fields
+
+class PredictionDetailsSchema(Schema):
+    class Meta:
+        ordered = True
+
+    class_name = fields.Str(required=True)
+    confidence = fields.Float(required=True)
+
+class ProbabilitiesSchema(Schema):
+    class Meta:
+        ordered = True
+
+    probabilities = fields.Dict(keys=fields.Str(), values=fields.Float())
+
+class PredictionResponseSchema(Schema):
+    predicted = fields.Nested(PredictionDetailsSchema, required=True)
+    probabilities = fields.Nested(ProbabilitiesSchema, required=True)
